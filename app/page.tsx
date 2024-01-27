aca79d6c6c574dea9a5b52dcdffe5435e02b0d77
fktona <adetona.fk@gmail.com>
2024-01-28 00:36:24 +0100

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,114 @@
-import Image from 'next/image'
+import fs from 'fs';
+import path from 'path';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { PromptTemplate } from 'langchain/prompts';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { StringOutputParser } from 'langchain/schema/output_parser';
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import { RunnableSequence } from '@langchain/core/runnables';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+type PromptTemplateInput<T, U> = {
+  prompt: string;
+  // Add other properties if needed
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+export default  function Home() {
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 300,
+    separators: ['\n', ' ', '.', ',', '!', '?', ';', ':', '(', ')', '[', ']', '{', '}', '<', '>', '"', "'"],
+  });  
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  const openAIApiKey = process.env.openAIApiKey;
+  
+  const llm = new ChatOpenAI({ openAIApiKey });
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  const prompt: PromptTemplateInput<any, any> = {
+    prompt: `Given a question covert it to a standalone question.
+    question: {question}  standalone question`
+  };
+
+  const promptTemplate = PromptTemplate.fromTemplate(prompt.prompt);
+
+  const answer=`You are a  helpfull chatbot provided with some information  about a document based on the context provided.
+     You are asked to answer some questions about the story. always reply nicely and politely.
+     context: {context} question: {question} answer:`
+  const answerPromt = PromptTemplate.fromTemplate(answer);
+
+  const grammer = `correct the given sentence.
+    sentence: {sentence} corrected sentence:`
+
+    const grammerPrompt = PromptTemplate.fromTemplate(grammer);
+
+    const chain = RunnableSequence.from([grammerPrompt, llm, answerPromt, new StringOutputParser()])
+  const embeddings = new OpenAIEmbeddings({ openAIApiKey });
+
+
+   const sbUrl = process.env.supabaseUrl;
+     const supabaseKey = process.env.supabaseKey;
+
+      const client = createClient(sbUrl ?? '', supabaseKey ?? '');
+   const load = async () => {
+    const loader = new PDFLoader("public/story.pdf");
+
+    const docs = await loader.load();
+    //console.log('ghyh', docs)
+
+    // await SupabaseVectorStore.fromDocuments(docs, 
+    //   new OpenAIEmbeddings({ openAIApiKey }),{
+    //     client,
+    //     tableName: 'documents'
+    //   });
+    //   console.log('done')
+
+    const vectorstores = new SupabaseVectorStore(embeddings,{
+      client,
+      tableName: 'documents',
+      //queryName:'langchain query'
+    });
+
+
+    const combined = () => {
+      const combined = docs.map((doc) => doc.pageContent).join('\n\n');
+      return combined;
+    }
+
+    
+    const  retrival =  vectorstores.asRetriever()
+    // const ask  = await retrival.invoke('The Boscombe Valley Mystery');
+    // console.log(ask);
+    const result =  promptTemplate.pipe(llm).pipe(new StringOutputParser()).pipe(retrival).pipe(combined).pipe(answerPromt);
+     const text = await result.invoke({  question: 'give the summary of the story' });
+   console.log(text);
+    
+   
+    //console.log(text);
+   }
+
+   
+
+   const file = async () => {
+  //   const filePath = path.join(process.cwd(), 'public', 'faith.txt');
+  //   const fileContents = fs.readFileSync(filePath, 'utf8');
+  //   const output = await splitter.createDocuments([fileContents]);
+  //   const embeddingsResult = await embeddings.embedQuery(fileContents)
+    
+
+    
+  //   return fileContents;
+
+
+   }
+
+file();
+
+
+    
+
+  return load();
 }
+      
